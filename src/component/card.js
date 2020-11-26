201c4a54c357728c7c8fc08cf3d1ae025f9d0641
KykiSemyen <iam-kyki@mail.ru>
2020-11-26 16:19:37 +0600

    done

--- src/component/card.js
+++ src/component/card.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
+import "./card.css";
 
 function Card(params) {
-  const [like, setLike] = useState(false);
+  const [like, setLike] = useState(params.like);
 
   console.log("like", like);
 
   return (
     <div className="card">
-      <div className="card_img">Здесь картинка</div>
-      <div className="card_title">Это НАЗВАНИЕ</div>
-      <div className="card_price"> ЭТО ЦЕНА </div>
-      <div onClick={() => setLike(!like)}>
+      <div className="card_img"></div>
+      <div className="card_title">{params.title}</div>
+      <div className="card_price">{params.price}</div>
+      <div
+        style={like ? { color: "red" } : { color: "black" }}
+        onClick={() => setLike(!like)}
+        className="like"
+      >
         <div>лайк</div>
       </div>
     </div>
--- src/page/main.js
+++ src/page/main.js
@@ -1,10 +1,34 @@
 import React from "react";
 import Card from "../component/card";
+import "./main.css";
+import home from "../img/house.png";
+
+const data = [
+  { id: 1, name: "ЖК Первый", img: home, price: "200K", like: false },
+  { id: 2, name: "ЖК Второй", img: home, price: "200K", like: false },
+  { id: 3, name: "ЖК Третий", img: home, price: "200K", like: false },
+  { id: 4, name: "ЖК Четвертый", img: home, price: "200K", like: false },
+  { id: 5, name: "ЖК Пятый", img: home, price: "200K", like: false },
+];
 
 function Main() {
+  console.log(home);
+
+  let cards = data.map((item) => (
+    <Card
+      key={item.id}
+      title={item.name}
+      img={item.home}
+      price={item.price}
+      like={item.like}
+    />
+  ));
+
   return (
-    <div>
-      <Card />
+    <div className="main_page">
+      <div className="container">
+        <div className="main_content">{cards}</div>
+      </div>
     </div>
   );
 }
